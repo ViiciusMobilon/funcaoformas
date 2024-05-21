29a6d882e8579035c7a79f2fe394265ee3ccf733
Usuario <Usuario@CPD03-M12>
2024-05-21 12:28:29 -0300

    fim

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,15 +11,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Digite se é quadrado - trapésio - retangulo - Losango");
+            Console.WriteLine("Digite se é quadrado - trapézio - retangulo - Losango");
            string forma = Console.ReadLine().ToLower();
           switch (forma)
             {
                 case "quadrado":
                     Qua();
                     break;
-                case "trapesio":
-                    trapesio();
+                case "trapezio":
+                    trapezio();
                     break;
                 case "retangulo":
                     retangulo();
@@ -48,7 +48,7 @@ namespace ConsoleApp1
             Console.WriteLine("A area do quadrado é: " + area);
         }
 
-        static void trapesio() 
+        static void trapezio() 
         {
             Console.WriteLine("Digite a altura");
             double altura = double.Parse(Console.ReadLine());
@@ -59,7 +59,7 @@ namespace ConsoleApp1
             Console.WriteLine("Digite o segundo lado");
             double lado2 = double.Parse(Console.ReadLine());
             double area = (lado1 + lado2) * altura / 2;
-            Console.WriteLine("A area do trapésio é "+ area);
+            Console.WriteLine("A area do trapézio é "+ area);
         }
         static void retangulo()
         {
