979d2db9cbba1d65c66b910e43a581e02cc747c2
Usuario <Usuario@CPD03-M12>
2024-05-21 11:42:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite se é quadrado - trapésio - retangulo - Losango");
+           string forma = Console.ReadLine().ToLower();
+          switch (forma)
+            {
+                case "quadrado":
+                    Qua();
+                    break;
+                case "trapesio":
+                    trapesio();
+                    break;
+                case "retangulo":
+                    retangulo();
+                    break;
+                case "losangulo":
+                    losangulo();
+                    break;
+            }
+
+
+            Console.ReadKey();
+            
+           
+        }
+
+         static void Qua()
+        {
+            
+           Console.WriteLine("Digite a altura");
+           double altura= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado");
+            double lado = double.Parse(Console.ReadLine());
+
+            double area = lado * altura;
+            Console.WriteLine("A area do quadrado é: " + area);
+        }
+
+        static void trapesio() 
+        {
+            Console.WriteLine("Digite a altura");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro lado");
+            double lado1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo lado");
+            double lado2 = double.Parse(Console.ReadLine());
+            double area = (lado1 + lado2) * altura / 2;
+            Console.WriteLine("A area do trapésio é "+ area);
+        }
+        static void retangulo()
+        {
+
+            Console.WriteLine("Digite a altura");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado");
+            double lado = double.Parse(Console.ReadLine());
+
+            double area = lado * altura;
+            Console.WriteLine("A area do retangulo é: " + area);
+            
+        
+        }
+        static void losangulo()
+        {
+            Console.WriteLine("Digite a altura");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro lado");
+            double lado = double.Parse(Console.ReadLine());
+
+            double area = (lado * altura ) / 2;
+            Console.WriteLine("A area do losangulo "+area);
+        }
+    }
+}
